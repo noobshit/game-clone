cb426b344d529e46752f41734635188c69b18369
Andżej <trader1999@o2.pl>
2019-04-30 15:10:22 +0200

    stackable

--- server/box.js
+++ server/box.js
@@ -136,9 +136,45 @@ function create_enlargment() {
     )
 }
 
+function stackable(entity, stack_amount=1, stack_limit=16) {
+    const state = {
+        stack_amount,
+        stack_limit,
+        stack_with(target) {
+            const [bigger, smaller] = [entity, target].sort((a,b) => b.stack_amount - a.stack_amount)
+            const move_amount = Math.min(
+                bigger.stack_limit - bigger.stack_amount,
+                smaller.stack_amount
+            )
 
-function create_explo() {
-    return create_box('explo.png')
+            if (move_amount > 0) {
+                smaller.stack_amount -= move_amount
+                bigger.stack_amount += move_amount
+
+                if (smaller.stack_amount == 0) {
+                    smaller.parent.remove_entity(smaller)
+                }
+            }
+        }
+    }
+
+    entity.events.on('collision_start', function(event) {
+        if (event.collided_with.type === entity.type) {
+            entity.stack_with(event.collided_with)
+        }
+    })
+
+    return state
+}
+
+function create_explo(amount=1) {
+    const box = create_box('explo.png')
+    box.type = 'explo'
+    
+    return Object.assign(
+        box,
+        stackable(box, amount, 16)
+    )
 }
 
 
